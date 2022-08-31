a1f878d88caaa0eb76b9adc77ea991c245d283df
danielNishimura <nishimurani@gmail.com>
2022-08-31 08:28:55 -0300

    view dinamico

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -6,11 +6,32 @@ use \core\Controller;
 class HomeController extends Controller {
 
     public function index() {
-        $this->render('home', ['nome' => 'Daniel Nishimura']);
+        $nome = 'Daniel Nishimura';
+
+        $posts = [
+            ['titulo' => 'Titulo para teste0', 'corpo' => 'Corpo testando0'],
+            ['titulo' => 'Titulo para teste1', 'corpo' => 'Corpo testando1'],
+            ['titulo' => 'Titulo para teste2', 'corpo' => 'Corpo testando2'],
+            ['titulo' => 'Titulo para teste3', 'corpo' => 'Corpo testando3'],
+            ['titulo' => 'Titulo para teste4', 'corpo' => 'Corpo testando4'],
+            ['titulo' => 'Titulo para teste5', 'corpo' => 'Corpo testando5'],
+            ['titulo' => 'Titulo para teste6', 'corpo' => 'Corpo testando6'],
+        ];
+
+        $this->render('home', [
+            'nome' => $nome,
+            'idade' => 45,
+            'posts' => $posts
+        ]);
     }
 
     public function fotos() {
         $this->render('fotos');
+    }
+
+
+    public function foto($parametros) {
+        echo "Acessando a foto:".$parametros['id'];
     }
 
     public function sobre() {
@@ -20,5 +41,4 @@ class HomeController extends Controller {
     public function sobreP($args) {
         print_r($args);
     }
-
 }
--- src/routes.php
+++ src/routes.php
@@ -5,5 +5,6 @@ $router = new Router();
 
 $router->get('/', 'HomeController@index');
 $router->get('/fotos', 'HomeController@fotos');
+$router->get('/foto/{id}', 'HomeController@foto');
 $router->get('/sobre/{nome}', 'HomeController@sobreP');
 $router->get('/sobre', 'HomeController@sobre');
